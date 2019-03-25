a3446687a3d439cea3dbb74c16a73782bfac47a2
acovalero <acovalero18@gmail.com>
2019-03-25 14:05:29 +0800

    A63

--- database/seeds/sql/plantreports.sql
+++ database/seeds/sql/plantreports.sql
@@ -1,31 +1,31 @@
 -- id, active, seasons_id, created_at,updated_at
 
 -- season 1
-INSERT INTO plant_reports VALUES (1,Null,1,'2017-07-24','2017-07-24');
-INSERT INTO plant_reports VALUES (2,Null,1,'2017-08-24','2017-08-24');
-INSERT INTO plant_reports VALUES (3,Null,1,'2017-09-24','2017-09-24');
-INSERT INTO plant_reports VALUES (4,Null,1,'2017-10-01','2017-10-01');
+INSERT INTO plant_reports VALUES (1,'0',1,'2017-07-24','2017-07-24');
+INSERT INTO plant_reports VALUES (2,'0',1,'2017-08-24','2017-08-24');
+INSERT INTO plant_reports VALUES (3,'0',1,'2017-09-24','2017-09-24');
+INSERT INTO plant_reports VALUES (4,'0',1,'2017-10-01','2017-10-01');
 
 -- season 2
-INSERT INTO plant_reports VALUES (5,Null,2,'2017-11-01','2017-11-01');
-INSERT INTO plant_reports VALUES (6,Null,2,'2017-12-24','2017-12-24');
-INSERT INTO plant_reports VALUES (7,Null,2,'2018-01-24','2018-01-24');
-INSERT INTO plant_reports VALUES (8,Null,2,'2018-02-18','2018-02-18');
+INSERT INTO plant_reports VALUES (5,'0',2,'2017-11-01','2017-11-01');
+INSERT INTO plant_reports VALUES (6,'0',2,'2017-12-24','2017-12-24');
+INSERT INTO plant_reports VALUES (7,'0',2,'2018-01-24','2018-01-24');
+INSERT INTO plant_reports VALUES (8,'0',2,'2018-02-18','2018-02-18');
 
 -- season 3
-INSERT INTO plant_reports VALUES (9,Null,3,'2018-03-12','2018-03-12');
-INSERT INTO plant_reports VALUES (10,Null,3,'2018-04-12','2018-04-12');
-INSERT INTO plant_reports VALUES (11,Null,3,'2018-05-12','2018-05-12');
-INSERT INTO plant_reports VALUES (12,Null,3,'2018-06-28','2018-06-28');
+INSERT INTO plant_reports VALUES (9,'0',3,'2018-03-12','2018-03-12');
+INSERT INTO plant_reports VALUES (10,'0',3,'2018-04-12','2018-04-12');
+INSERT INTO plant_reports VALUES (11,'0',3,'2018-05-12','2018-05-12');
+INSERT INTO plant_reports VALUES (12,'0',3,'2018-06-28','2018-06-28');
 
 -- season 4
-INSERT INTO plant_reports VALUES (13,Null,4,'2018-07-18','2018-07-18');
-INSERT INTO plant_reports VALUES (14,Null,4,'2018-08-24','2018-08-24');
-INSERT INTO plant_reports VALUES (15,Null,4,'2018-09-24','2018-09-24');
-INSERT INTO plant_reports VALUES (16,Null,4,'2018-10-20','2018-10-20');
+INSERT INTO plant_reports VALUES (13,'0',4,'2018-07-18','2018-07-18');
+INSERT INTO plant_reports VALUES (14,'0',4,'2018-08-24','2018-08-24');
+INSERT INTO plant_reports VALUES (15,'0',4,'2018-09-24','2018-09-24');
+INSERT INTO plant_reports VALUES (16,'0',4,'2018-10-20','2018-10-20');
 
 -- season 5
-INSERT INTO plant_reports VALUES (17,Null,5,'2018-12-10','2018-12-10');
-INSERT INTO plant_reports VALUES (18,Null,5,'2019-01-24','2019-01-24');
-INSERT INTO plant_reports VALUES (19,Null,5,'2019-02-24','2019-02-24');
-INSERT INTO plant_reports VALUES (20,Null,5,'2019-03-26','2019-03-26');
+INSERT INTO plant_reports VALUES (17,'0',5,'2018-12-10','2018-12-10');
+INSERT INTO plant_reports VALUES (18,'0',5,'2019-01-24','2019-01-24');
+INSERT INTO plant_reports VALUES (19,'0',5,'2019-02-24','2019-02-24');
+INSERT INTO plant_reports VALUES (20,'0',5,'2019-03-26','2019-03-26');
