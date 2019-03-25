6fd08aab601d665afdca7ebbc5b88bf288f555b4
acovalero <acovalero18@gmail.com>
2019-03-25 14:23:13 +0800

    A64

---
+++ database/seeds/sql/plantdata.sql
@@ -0,0 +1,7 @@
+-- id, plant_area, farmers, active, plant_reports_id,users_id,created_at,updated_at
+
+INSERT INTO plant_datas VALUES(1,'6.0','3','0','1','18',now(),now());
+INSERT INTO plant_datas VALUES(2,'2.5','3','0','1','10',now(),now());
+INSERT INTO plant_datas VALUES(3,'4.0','5','0','1','10',now(),now());
+INSERT INTO plant_datas VALUES(4,'2.0','2','0','1','12',now(),now());
+INSERT INTO plant_datas VALUES(5,'1.0','1','0','1','17',now(),now());
